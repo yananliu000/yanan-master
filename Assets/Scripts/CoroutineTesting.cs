37eeb41a58945e0769be5201766e95154e90df42
yananliu <yanan.liu0325@gmail.com>
2019-02-22 11:04:30 -0800

    add

--- Assets/Scripts/CoroutineTesting.cs
+++ Assets/Scripts/CoroutineTesting.cs
@@ -15,4 +15,26 @@ public class CoroutineTesting : MonoBehaviour
     {
         
     }
+
+    IEnumerator MoveToTarget(GameObject target, float stopDistance, float detectDistance, float speed)
+    {
+        //get the distance bewteen self and target
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+
+        //not arrive or not detect
+        while (distanceToTarget >= stopDistance && distanceToTarget < detectDistance)
+        {
+            //the units per second to travel
+            float step = speed * Time.deltaTime;
+
+            //self will look at the target
+            transform.LookAt(target.transform.position);
+
+            //move 
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+            distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+            yield return null; //pause until next frame
+        }
+        yield return null;
+    }
 }
--- Assets/Scripts/ExtensionTesting.cs
+++ Assets/Scripts/ExtensionTesting.cs
@@ -2,4 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class ExtensionTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.ResetTransformation();
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+//Extension Methods!
+//must be static: both class and function
+//this representes where the original function come from 
+public static class ExtensionMethods
+{
+    //reset the obj position to user-defined v3
+    public static void ResetTransformation(this Transform trans)
+    {
+        Debug.Log("extented");
+        trans.position = Vector3.zero;
+        trans.localRotation = Quaternion.identity;
+
+        trans.localScale = new Vector3 (1,1,1);
+    }
+}
