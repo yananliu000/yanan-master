81c7eaee0a3a10d971678d405f105f0fffa31416
Yanan Liu <YLIU146@art.edu>
2019-02-22 13:45:49 -0800

    add

--- Assets/Scripts/LinqTesting.cs
+++ Assets/Scripts/LinqTesting.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; //must include to use the functionalities
 
 public class LinqTesting : MonoBehaviour
 {
+    private AttributeTesting[] others;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        others = GameObject.FindObjectsOfType<AttributeTesting>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //In LoL, some abilities will cause extra damage to low health characters 
+    public void GetHealthLowerThanValue(int value)
+    {
+        //create query
+        var Query =
+            from item in others
+            where item.health < value
+            select item;
+
+        //exe
+        foreach (var item in Query)
+        {
+            Debug.Log("Object: " + item.name + "'s health is below: " + value);
+        }
+    }
+
+    //is there any shortcut way to get the attribute range of health?
+    public void GetHealthHigherThanPercent(float percent)
+    {
+        //create query
+        //var Query =
+        //    from item in others
+        //    where item.health < 
+        //    select item;
+        //
+        ////exe
+        //foreach (var item in Query)
+        //{
+        //    Debug.Log("Object: " + item.name + "'s health is below: " + value);
+        //}
     }
 }
--- Assets/Scripts/MyPlayerController.cs
+++ Assets/Scripts/MyPlayerController.cs
@@ -9,29 +9,16 @@ public class MyPlayerController : MonoBehaviour
     public float moveSpeed = 6.0f;
     public float turnSpeed = 150.0f;
     public float jumpForce = 5.0f;
-    private Animator anim = null;
     bool IsGrounded = true;
-
-    [Header("Teleport Location Data")]
-    public Transform teleportPosition;
 
     private void Start()
     {
-        //get animator
-        anim = GetComponent<Animator>();
     }
+
     void Update()
     {
-        if(GroundCheck() && IsGrounded == false)
-            anim.SetTrigger("stopJump");
-
         if (Input.GetKeyDown(KeyCode.Space) && GroundCheck())
             Jump();
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            gameObject.transform.position = teleportPosition.position;
-        }
 
         Movement();
     }
@@ -42,7 +29,6 @@ public class MyPlayerController : MonoBehaviour
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        anim.SetFloat("moveSpeed", Mathf.Abs(Input.GetAxis("Vertical")));
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
     }
@@ -50,7 +36,6 @@ public class MyPlayerController : MonoBehaviour
     void Jump()
     {
         IsGrounded = false;
-        anim.SetTrigger("jump");
         GetComponent<Rigidbody>().AddForce(0, jumpForce, 0, ForceMode.Impulse);
     }
 
