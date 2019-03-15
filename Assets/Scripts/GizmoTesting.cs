d9509cfc7d43040a2eca598e98ccfc63c84b6588
Yanan Liu <YLIU146@art.edu>
2019-03-15 15:05:35 -0700

    1

--- Assets/Scripts/GizmoTesting.cs
+++ Assets/Scripts/GizmoTesting.cs
@@ -25,9 +25,26 @@ public class GizmoTesting : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
+        /*
         // Draws a 5 unit long red line in front of the object
         Gizmos.color = Color.red;
         Vector3 direction = transform.TransformDirection(Vector3.forward) * 5;
         Gizmos.DrawRay(transform.position, direction);
+        */
+
+        float totalFOV = 70.0f;
+        float rayRange = 10.0f;
+        float halfFOV = totalFOV / 2.0f;
+        Quaternion leftRayRotation = Quaternion.AngleAxis(-halfFOV, Vector3.up);
+        Quaternion rightRayRotation = Quaternion.AngleAxis(halfFOV, Vector3.up);
+        Vector3 leftRayDirection = leftRayRotation * transform.forward;
+        Vector3 rightRayDirection = rightRayRotation * transform.forward;
+        Gizmos.DrawRay(transform.position, leftRayDirection * rayRange);
+        Gizmos.DrawRay(transform.position, rightRayDirection * rayRange);
+
+        //Gizmos.DrawFrustum
+        //DrawSolidArc
+        //GUI.HorizontalSlider(new Rect(20, 20, 100, 40), m_FieldOfView, min, max);
+        //DrawGUITexture
     }
 }
