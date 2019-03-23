ddab7367a2ecad2d00f538b24699497f0d643bcc
yananliu <yanan.liu0325@gmail.com>
2019-03-23 00:01:43 -0700

    1
    1

---
+++ Assets/Scripts/unoptimizeTesting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unoptimizeTesting : MonoBehaviour
+{
+    public bool m_isActivated = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var list = GameObject.FindGameObjectsWithTag("Cube");
+        foreach (var a in list)
+        {
+            if (m_isActivated)
+            {
+                a.transform.position += new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+                var list2 = GameObject.FindObjectsOfType<CoroutineTesting>();
+                foreach (var e in list2)
+                {
+                    if (e.GetComponent<GizmoTesting>())
+                    {
+                        BroadcastMessage("BroadcastMe");
+                    }
+                    else
+                    {
+                        a.transform.position -= new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+                    }
+                }
+            }
+        }
+    }
+
+    public void BroadcastMe()
+    {
+        Debug.Log("unoptimizeTesting succeed");
+    }
+
+}
+
